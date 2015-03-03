6c1235303ca7a8566012b23197da8bbda7a9a152
Aaron Gray <aaron@hellometa.com>
2015-03-03 13:59:05 -0700

    Include return value in code comment

--- src/js/app.js
+++ src/js/app.js
@@ -5,9 +5,10 @@ let songs = [
 ];
 
 /**
- * Returns the total length of every song in the songs array.
+ * Accumulator returns the total length of every song in the songs array.
  *
  * @param  songs  An array of song objects where each has a length property.
+ * @return int    Sum of every integer from the length prop in the songs array.
  */
 const getLength = (songs) => {
   let length = 0;
