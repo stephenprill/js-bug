8efc2b8bc200af633ca90f041ab10a9209d491d8
Aaron Gray <aaron@hellometa.com>
2015-03-03 10:24:15 -0700

    Initial commit

---
+++ readme.md
@@ -0,0 +1,18 @@
+# Bugs:
+
+This project has a few bugs in it. Your mission is to figure out where things are going wrong.
+
+1. "Playlist Length is `undefined` seconds" should say the total song playlist length.
+2. Once the total length shows up, why is it such a large number? There are only 3 songs.
+3. Why is only one song showing up?
+
+### With Broken Code
+![Alt text](src/img/before.png)
+
+### With Fixed Code
+![Alt text](src/img/after.png)
+
+## Setup & Server
+1. Run `npm install`
+1. Run `npm run watch` to start a server and build assets.
+1. Visit localhost:8080
---
+++ src/js/app.js
@@ -0,0 +1,43 @@
+let songs = [
+  {
+    title: "Poor Song",
+    artist: "Yeah Yeah Yeahs",
+    length: 111
+  },
+  {
+    title: "Glowing",
+    artist: "D.A.",
+    length: 109
+  },
+  {
+    title: "17",
+    artist: "Youth Lagoon",
+    length: "120"
+  }
+];
+
+const getLength = (songs) => {
+  let length = 0;
+  songs.forEach(song => length += song.length);
+  return length;
+}
+
+const generateView = (template, data, id) => {
+  let dest = document.getElementById(id),
+      re = /<%([^%>]+)?%>/g,
+      match;
+
+  while(match = re.exec(template)) {
+    template = template.replace(match[0], data[match[1].trim('')]);
+  }
+
+  return dest.innerHTML = template;
+}
+
+songs.forEach( song => {
+  generateView(document.getElementById('song').innerHTML, song, 'songs');
+});
+
+generateView(document.getElementById('playlist').innerHTML, {
+  length: getLength(songs)
+}, 'footer');
